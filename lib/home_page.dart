a938248f83e3730870391c55072396e9857be178
derryraja <derry.raja@gmail.com>
2021-07-08 21:09:16 +0530

    Added Weather Forcast Functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:weathercast/Themes/app_color.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,15 +13,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int temperature = 0;
-  String location = 'Chennai';
+  int temperature = 19;
+  String location = 'San Francisco';
+  int woeid = 2487956;
+  String weather = 'lightcloud';
+  int wind = 8;
+  int humidity = 82;
+  int visibility = 7;
+
+  String searchApiUrl =
+      'https://www.metaweather.com/api/location/search/?query=';
+  String locationApiUrl = 'https://www.metaweather.com/api/location/';
+
+  void fetchSearch(String input) async {
+    var oriSearchResult = searchApiUrl + input;
+    var searchResult = await http.get(Uri.parse(oriSearchResult));
+    var result = json.decode(searchResult.body)[0];
+
+    setState(() {
+      location = result["title"];
+      woeid = result["woeid"];
+    });
+  }
+
+  void fetchLocation() async {
+    var oriLocationResult = locationApiUrl + woeid.toString();
+    var locationResult = await http.get(Uri.parse(oriLocationResult));
+    var result = json.decode(locationResult.body);
+    var consolidated_weather = result["consolidated_weather"];
+    var data = consolidated_weather[0];
+    setState(() {
+      temperature = data["the_temp"].round();
+      weather = data["weather_state_name"].replaceAll(' ', '').toLowerCase();
+      wind = data["wind_speed"].round();
+      humidity = data["humidity"];
+      visibility = data["visibility"].round();
+    });
+  }
+
+  void onTextFieldSubmitted(String input) {
+    fetchSearch(input);
+    fetchLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/clear.png'),
+          image: AssetImage('assets/$weather.png'),
           fit: BoxFit.cover,
         ),
       ),
@@ -62,7 +104,7 @@ class _HomePageState extends State<HomePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'Today',
+                        weather,
                         style: GoogleFonts.montserrat(
                             fontSize: 24,
                             fontWeight: FontWeight.normal,
@@ -89,23 +131,165 @@ class _HomePageState extends State<HomePage> {
                                   fontWeight: FontWeight.normal,
                                   color: AppColor.kwhite),
                             ),
-                          ])
+                          ]),
+                    ],
+                  ),
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.only(top: 40),
+                //   child: Row(
+                //     children: [
+                //       Text(
+                //         'Today',
+                //         style: GoogleFonts.montserrat(
+                //             fontSize: 16,
+                //             fontWeight: FontWeight.normal,
+                //             color: AppColor.kwhite),
+                //       ),
+                //       SizedBox(
+                //         width: 20,
+                //       ),
+                //       Text(
+                //         'Tomorrow',
+                //         style: GoogleFonts.montserrat(
+                //             fontSize: 16,
+                //             fontWeight: FontWeight.normal,
+                //             color: AppColor.klightGrey),
+                //       ),
+                //       SizedBox(
+                //         width: 20,
+                //       ),
+                //       Text(
+                //         'Next 7 days',
+                //         style: GoogleFonts.montserrat(
+                //             fontSize: 16,
+                //             fontWeight: FontWeight.normal,
+                //             color: AppColor.klightGrey),
+                //       ),
+                //       SizedBox(
+                //         width: 5,
+                //       ),
+                //       Icon(
+                //         Icons.arrow_right,
+                //         color: AppColor.klightGrey,
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 200),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Wind',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            wind.toString(),
+                            style: GoogleFonts.montserrat(
+                                fontSize: 30,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'mph',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.klightGrey),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Humidity',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            humidity.toString(),
+                            style: GoogleFonts.montserrat(
+                                fontSize: 30,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            '%',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.klightGrey),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Visibility',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            visibility.toString(),
+                            style: GoogleFonts.montserrat(
+                                fontSize: 30,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.kwhite),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Miles',
+                            style: GoogleFonts.montserrat(
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                                color: AppColor.klightGrey),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(top: 150),
+                  padding: const EdgeInsets.only(top: 40),
                   child: TextField(
+                    onSubmitted: (String input) {
+                      onTextFieldSubmitted(input);
+                    },
                     style: TextStyle(
-                      color: AppColor.kprimaryColor,
+                      color: AppColor.kwhite,
                     ),
                     keyboardType: TextInputType.name,
-                    onChanged: (value) {
-                      setState(() {
-                        //email = value;
-                      });
-                    },
-                    //textAlign: TextAlign.start,
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.transparent,
@@ -130,151 +314,6 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Today',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            color: AppColor.kwhite),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        'Tomorrow',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            color: AppColor.klightGrey),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        'Next 7 days',
-                        style: GoogleFonts.montserrat(
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal,
-                            color: AppColor.klightGrey),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Icon(
-                        Icons.arrow_right,
-                        color: AppColor.klightGrey,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Wind',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '8',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 30,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            'km/h',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.klightGrey),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Humidity',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '82',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 30,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '%',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.klightGrey),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            'Rain',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '7',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 30,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.kwhite),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            '%',
-                            style: GoogleFonts.montserrat(
-                                fontSize: 14,
-                                fontWeight: FontWeight.normal,
-                                color: AppColor.klightGrey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
               ],
             ),
           ),
